ab6fe71b53309ca040e3f177821ee1c4a57eae7b
ShintaroYamamoto <fourthman_shin@yahoo.co.jp>
2024-09-17 20:11:11 +0900

    ホーム修正

--- js/home.js
+++ js/home.js
@@ -1,27 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const visionSection = document.querySelector('.vision-section');
-    const missionSection = document.querySelector('.mission-section');
-    const valueSection = document.querySelector('.value-section'); // VALUEセクションを追加
 
     function onScroll() {
         const sectionTopVision = visionSection.getBoundingClientRect().top;
-        const sectionTopMission = missionSection.getBoundingClientRect().top;
-        const sectionTopValue = valueSection.getBoundingClientRect().top; // VALUEセクションの位置を取得
         const sectionHeight = window.innerHeight;
 
         // VISIONセクションのアニメーション
         if (sectionTopVision < sectionHeight - 100) {
             visionSection.classList.add('visible');
-        }
-
-        // MISSIONセクションのアニメーション
-        if (sectionTopMission < sectionHeight - 100) {
-            missionSection.classList.add('visible');
-        }
-
-        // VALUEセクションのアニメーション
-        if (sectionTopValue < sectionHeight - 100) {
-            valueSection.classList.add('visible');
         }
     }
 
