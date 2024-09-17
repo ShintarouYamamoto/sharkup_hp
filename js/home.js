5af721e5dd913d759c175eea532cfbaa4fa681ae
Shintaro <fourthman_shin@yahoo.co.jp>
2024-09-17 22:33:16 +0900

    ナビバー一部修正、ホームページ修正

--- js/home.js
+++ js/home.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     const visionSection = document.querySelector('.vision-section');
+    const businessSection = document.querySelector('.business-section');
 
     function onScroll() {
         const sectionTopVision = visionSection.getBoundingClientRect().top;
+        const sectionTopBusiness = businessSection.getBoundingClientRect().top;
         const sectionHeight = window.innerHeight;
 
         // VISIONセクションのアニメーション
         if (sectionTopVision < sectionHeight - 100) {
             visionSection.classList.add('visible');
+        }
+
+        // BUSINESSセクションのアニメーション
+        if (sectionTopBusiness < sectionHeight - 100) {
+            businessSection.classList.add('visible');
         }
     }
 
